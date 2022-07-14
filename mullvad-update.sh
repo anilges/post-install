4aa527c90ac70ad4eb987906f415b04393eeeacb
Alexander Nilges <nilges.alex@gmx.de>
2022-07-14 23:20:30 +0200

    back

--- mullvad-update.sh
+++ mullvad-update.sh
@@ -3,8 +3,6 @@
 cd $HOME
 # install newest mullvad
 wget --content-disposition https://mullvad.net/download/app/deb/latest
-sudo chown -Rv _apt:root MullvadVPN*
-sudo chmod -Rv 700 MullvadVPN*
 sudo apt install -y ./MullvadVPN*
 
 # remove deb
