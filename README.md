e9183cf786efb28e2dbfedd109dfd4c10dc19a07
Alexander Nilges <nilges.alex@gmx.de>
2022-07-14 21:45:21 +0200

    few fixes + obs-ndi

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # post-install
+
+sudo apt update && sudo apt install git
+git clone https://github.com/anilges/post-install.git
+chmod +x $HOME/post-install/ubuntu-post-install.sh && bash ubuntu-post-install.sh
+
+after that:
+    - firefox privacy guides + extensions (ublock, bitwarden, simplelogin, leechblock:github distracting websites)
+    - settings: dark mode, home folder uncheck, default apps, terminal bell off
+    - codium: vim, latex workshop
+    - connect github ssh
+    - mullvad login
+    - thunderbird login
+    - 
--- ubuntu-post-install.sh
+++ ubuntu-post-install.sh
@@ -6,19 +6,27 @@ sudo apt update && sudo apt upgrade -y
 sudo snap refresh
 
 # install apt programs
-sudo add-apt-repository universe
-sudo apt install ubuntu-restricted-extras texlive-full libfuse2 jq -y
+sudo add-apt-repository universe -y
+sudo add-apt-repository ppa:obsproject/obs-studio -y
+sudo apt update
+sudo apt install ubuntu-restricted-extras texlive-full libfuse2 jq ffmpeg obs-studio -y
+
+# install obs-ndi
+wget https://github.com/Palakis/obs-ndi/releases/download/4.9.1/libndi4_4.5.1-1_amd64.deb
+wget https://github.com/Palakis/obs-ndi/releases/download/4.9.1/obs-ndi_4.9.1-1_amd64.deb
+sudo apt install -y ./libndi4_4* ./obs-ndi_4*
+rm -rf libndi4_4* obs-ndi_4*
 
 # install vscodium
-snap install codium --classic
+sudo snap install codium --classic
 
 # create .bash_aliases
 cd $HOME
 touch .bash_aliases
 echo "alias upsys='sudo apt update && sudo apt upgrade -y && sudo snap refresh'" >> .bash_aliases
-echo "alias uplog='./$HOME/post-install/logseq-update.sh'" >> .bash_aliases
-echo "alias upnvim='./$HOME/post-install/neovim-update.sh'" >> .bash_aliases
-echo "alias upvpn='./$HOME/post-install/mullvad-update.sh'" >> .bash_aliases
+echo "alias uplog='$HOME/post-install/logseq-update.sh'" >> .bash_aliases
+echo "alias upnvim='$HOME/post-install/neovim-update.sh'" >> .bash_aliases
+echo "alias upvpn='$HOME/post-install/mullvad-update.sh'" >> .bash_aliases
 
 # create ssh key
 yes "" | ssh-keygen -t ed25519 -C "nilges.alex@gmx.de"
