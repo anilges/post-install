d96fdf191acf93098803e92982d43ce33b8f8913
Alexander Nilges <48833017+anilges@users.noreply.github.com>
2022-07-16 12:04:15 +0200

    Update logseq-update.sh

--- logseq-update.sh
+++ logseq-update.sh
@@ -14,4 +14,4 @@ rm -rf $HOME/AppImages/Logseq*
 cd $HOME/AppImages/
 latest_logseq=$(curl -s https://api.github.com/repos/logseq/logseq/releases/latest | jq -r ".tag_name")
 wget https://github.com/logseq/logseq/releases/download/$latest_logseq/Logseq-linux-x64-$latest_logseq.AppImage
-chmod +x Logseq*
+chmod +x Logseq-linux-x64-$latest_logseq.AppImage
